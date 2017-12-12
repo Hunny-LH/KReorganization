d6ce2953e2633572c342f5718e6ddc9ec8705f92
Hunny-LH <393803588@qq.com>
2017-12-12 22:56:33 +0800

    JavaBaseDemo

---
+++ Code/JavaBaseDemo/src/com/github/hunnylh/Main.java
@@ -0,0 +1,25 @@
+package com.github.hunnylh;
+
+/**
+ * @author <a href="mailto: 393803588@qq.com">刘涵(Hanl)</a>
+ * By 2017/12/12
+ */
+public class Main {
+
+    /**
+     * <h1>在Java程序中，每一个可执行的Jar都有一个主线程的入口，即main方法。</h1>
+     * main方法的定义要点：
+     * <ul>
+     *     <li>public修饰：</li>
+     *     <li>static修饰：</li>
+     *     <li>void返回类型声明：</li>
+     *     <li>main方法名称：</li>
+     *     <li>args参数列表：</li>
+     * </ul>
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("I`m a Main Class.");
+    }
+}
