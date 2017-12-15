8516b07e374f09b0c5d93383f361c823b5209200
Hunny-LH <393803588@qq.com>
2017-12-15 21:51:37 +0800

    Java基础

---
+++ Code/JavaAPIDemo/src/com/github/hunnylh/stream/StreamApi.java
@@ -0,0 +1,21 @@
+package com.github.hunnylh.stream;
+
+import java.util.stream.Stream;
+
+import static com.github.hunnylh.common.PerfUtils.perfInvoke;
+
+/**
+ * @author <a href="mailto: 393803588@qq.com">刘涵(Hanl)</a>
+ * By 2017/12/15
+ */
+public class StreamApi {
+
+    public static void main(String[] args) {
+        perfInvoke(() -> {
+            Stream.generate(() -> 1)
+                    .limit(100000)
+                    .map(integer -> integer + 1)
+                    .forEach(System.out::println);
+        });
+    }
+}
