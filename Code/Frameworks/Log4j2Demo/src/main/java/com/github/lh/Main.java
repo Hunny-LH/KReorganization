eba7430fa0a46336924da5234787d58998fce14e
Hunny-LH <393803588@qq.com>
2017-12-21 11:47:59 +0800

    log4j demo

---
+++ Code/Frameworks/Log4j2Demo/src/main/java/com/github/lh/Main.java
@@ -0,0 +1,15 @@
+package com.github.lh;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author <a href="mailto:393803588@qq.com">HanL(liuhan3)</a>
+ * @date 17-12-21
+ */
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) {
+        log.info("Hello Liuhan");
+    }
+}
